2ed7e346046f2198268f6ab693cd82526617e4cc
Ruby.git_use <ubuntu@ip-172-31-17-46.ap-northeast-1.compute.internal>
2021-06-05 08:43:57 +0000

    tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_080450) do
+ActiveRecord::Schema.define(version: 2021_06_05_071438) do
 
-  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
